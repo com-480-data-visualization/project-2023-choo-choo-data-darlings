68c778e0594aa95a04f6852ed0cc5629c4ecf2ac
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-03-26 14:58:26 +0200

    refacto into class

--- d3_map_example/js/map.js
+++ d3_map_example/js/map.js
@@ -1,95 +1,152 @@
-function load_map(
-    data_file,
-    width = 800,
-    height = 600,
-    scale = 9000,
-    center = [8, 47],
-    min_zoom_dimension = 100,
-    default_zone_color = "grey",
-) {
-    // Global state variables
-    let is_zoomed = false
-    let clicked_zone = null
+class SwissMap {
+    constructor(
+        map_file,
+        scale = 9000,
+        center = [8, 47],
+        min_zoom_dimension = 100,
+        default_zone_color = "grey",
+    ) {
+        this.map_file = map_file;
 
-    // Define projection
-    const projection = d3.geoMercator()
-        .center(center)
-        .scale(scale)
-        .translate([width / 2, height / 2])
+        // Basic render settings
+        this.scale = scale;
+        this.center = center;
+        this.min_zoom_dimension = min_zoom_dimension;
+        this.default_zone_color = default_zone_color;
+        
+        // State variables
+        this.is_zoomed = false;
+        this.clicked_zone = null;
 
-    // Define path generator
-    const path_generator = d3.geoPath().projection(projection);
+        // Initialize map
+        this.svg = this.init_svg();
+        this.g = this.init_g();
+        this.projection = this.init_projection();
+        this.zoom = this.init_zoom();
+    }
 
-    // Create SVG
-    const svg = d3.select("svg")
-        .attr("width", 800)
-        .attr("height", 600)
+    init_svg() {
+        const svg = d3.select("svg");
+        return svg;
+    }
 
-    const g = svg.append("g")
+    init_projection() {
+        const projection = d3.geoMercator()
+            .center(this.center)
+            .scale(this.scale)
+            .translate([this.svg_width / 2, this.svg_height / 2]);
+        return projection;
+    }
 
-    // Load data
-    d3.json(data_file).then(function(data) {
+    init_g() {
+        const g = this.svg.append("g");
+        return g;
+    }
 
-        g.selectAll("path")
-            .data(data.features)
-            .enter()
-            .append("path")
-            .attr("fill", default_zone_color)
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .style("stroke", "white")
-            .style("stroke-width", 0.2) 
-
-        // On hover of a country, change the color
-        .on("mouseover", function(d) {
-            onMouseOverZone(this)
-        })
-        .on("mouseout", function(d) {
-            onMouseOutZone(this)
-        })
-        .on("click", function(d) {
-            zoom.filter(() => is_zoomed)
-            if (is_zoomed) {
-                zoomOut()
-            } else {
-                zoomOnZone(this)
-            }
-            is_zoomed = !is_zoomed
-            if (!is_zoomed) {
-                resetZone(clicked_zone)
-            }
-            clicked_zone = is_zoomed ? this : null
-        })
-    });
-
-    // Define zoom and drag behavior
-    const zoom = d3.zoom()
+    init_zoom() {
+        const zoom = d3.zoom()
         .scaleExtent([1, 8])
-        .on("zoom", function({transform}) {
-            g.attr("transform", transform);
+        .on("zoom", ({transform}) => {
+            this.g.attr("transform", transform);
         });
 
-    svg.call(zoom);
+        this.svg.call(zoom);
 
-    function onMouseOverZone(zone) {
-        if (is_zoomed) { return }
-
-        fadeToColor(zone, "red")
-
-        const zone_center = getZoneCenter(zone)
-        addZoneTitle(zone, zone_center[0], zone_center[1])
+        return zoom;
     }
 
-    function onMouseOutZone(zone) {
-        if (is_zoomed) { return }
-
-        fadeToColor(zone, default_zone_color)
-        removeZoneTitle()
+    get svg_width() {
+        const svgNode = this.svg.node();
+        return svgNode.getBoundingClientRect().width;
     }
 
-    function fadeToColor(zone, color) {
-        if (is_zoomed) { return }
+    get svg_height() {
+        const svgNode = this.svg.node();
+        return svgNode.getBoundingClientRect().height;
+    }
+
+    get path_generator() {
+        return d3.geoPath().projection(this.projection);
+    }
+
+    load_data() {
+        d3.json(this.map_file).then(data => {
+            this.g.selectAll("path")
+                .data(data.features)
+                .enter()
+                .append("path")
+                .attr("fill", this.default_zone_color)
+                .attr("d", d3.geoPath()
+                    .projection(this.projection)
+                )
+                .style("stroke", "white")
+                .style("stroke-width", 0.2) 
+                .on("mouseover", d => {
+                    this.onMouseOverZone(d.target)
+                })
+                .on("mouseout", d => {
+                    this.onMouseOutZone(d.target)
+                })
+                .on("click", d => {
+                    if (this.is_zoomed) {
+                        this.zoomOut()
+                    } else {
+                        this.zoomOnZone(d.target)
+                    }
+                    this.is_zoomed = !this.is_zoomed
+                    this.zoom.filter(() => !this.is_zoomed) 
+                    if (!this.is_zoomed) {
+                        this.resetZone(this.clicked_zone)
+                    }
+                    this.clicked_zone = this.is_zoomed ? d.target : null
+                });
+    
+            const moudon_coordinates = [6.798421825543208, 46.66993202039939]
+            this.g.append("circle")
+                .attr("cx", this.projection(moudon_coordinates)[0])
+                .attr("cy", this.projection(moudon_coordinates)[1])
+                .attr("r", 2)
+                .attr("fill", "black")
+                .style("pointer-events", "none")
+            // Add moudon text
+            this.g.append("text")
+                .attr("x", this.projection(moudon_coordinates)[0])
+                .attr("y", this.projection(moudon_coordinates)[1])
+                .attr("dy", -3)
+                .attr("text-anchor", "middle")
+                .text("Moudon")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "5px")
+                .attr("fill", "black")
+                .style("pointer-events", "none")
+        });
+    }
+
+    update_data(new_map_file) {
+        this.map_file = new_map_file;
+        this.g.selectAll("*").remove();
+        this.load_data();
+    }
+
+    onMouseOverZone(zone) {
+        if (this.is_zoomed) { return }
+
+        this.fadeToColor(zone, "red")
+
+        const result = this.getZoneCenter(zone)
+        const zone_center = result[0]
+        this.addZoneTitle(zone, zone_center[0], zone_center[1])
+    }
+
+    onMouseOutZone(zone) {
+        if (this.is_zoomed) { return }
+
+        this.fadeToColor(zone, this.default_zone_color)
+        this.removeZoneTitle()
+    }
+
+    fadeToColor(zone, color) {
+        if (this.is_zoomed) { return }
 
         d3.select(zone)
             .transition()
@@ -97,66 +154,7 @@ function load_map(
             .attr("fill", color)
     }
 
-    function getZoneCenter(zone, sample_size = 10) {
-        // Necessary computation to handle the data structure
-        const zone_coordinates_arrays = d3.select(zone).data()[0].geometry.coordinates
-        var coordinates_total_counts_arrays = []
-        var coordinates_total_count = 0
-        zone_coordinates_arrays.forEach(function(coordinates_array) {
-            coordinates_total_counts_arrays.push(coordinates_array.length)
-            coordinates_total_count += coordinates_array.length
-        })
-
-        // Sample some coordinates and compute the center of the zone
-        var center = [0, 0]
-        var num_coordinates_sampled = 0
-        const step = Math.floor(coordinates_total_count / sample_size)
-        for (var i = 0; i < coordinates_total_count; i += step) {
-            // Find the coordinates array that contains the i-th coordinate
-            var coordinates_array_idx = 0
-            var acc_coordinates_count = coordinates_total_counts_arrays[0]
-            while (i >= acc_coordinates_count) {
-                coordinates_array_idx += 1
-                acc_coordinates_count += coordinates_total_counts_arrays[coordinates_array_idx]
-            }
-
-            // Find the location of the i-th coordinate in the coordinates array
-            var coordinates_idx = i - (acc_coordinates_count - coordinates_total_counts_arrays[coordinates_array_idx])
-
-            // Add the coordinates to the center
-            center[0] += zone_coordinates_arrays[coordinates_array_idx][coordinates_idx][0]
-            center[1] += zone_coordinates_arrays[coordinates_array_idx][coordinates_idx][1]
-
-            // Increment the number of coordinates sampled
-            num_coordinates_sampled += 1
-        }
-
-        // Compute the average
-        center[0] /= num_coordinates_sampled
-        center[1] /= num_coordinates_sampled
-
-        return center
-    }
-
-    function addZoneTitle(zone, lat, long) {
-        const zone_title = d3.select(zone).data()[0].properties.NAME
-        g.append("text")
-            .attr("x", projection([lat, long])[0])
-            .attr("y", projection([lat, long])[1])
-            .attr("text-anchor", "middle")
-            .attr("id", "zone_name")
-            .text(zone_title)
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "20px")
-            .attr("fill", "black")
-            .style("pointer-events", "none")
-    }
-
-    function removeZoneTitle() {
-        d3.select("#zone_name").remove()
-    }
-
-    function zoomOnZone(zone) {
+    getZoneCenter(zone, project, return_borders) {
         const zone_coordinates_arrays = d3.select(zone).data()[0].geometry.coordinates
         const most_bottoms = []
         const most_lefts = []
@@ -176,42 +174,73 @@ function load_map(
         const most_top = d3.max(most_tops)
 
         // Place points at locations
-        const bottom_left = projection([most_left, most_bottom])
-        const bottom_right = projection([most_right, most_bottom])
-        const top_left = projection([most_left, most_top])
-
-        let zone_width = Math.abs(bottom_right[0] - bottom_left[0])
-        zone_width = Math.max(zone_width, min_zoom_dimension)
-        let zone_height = Math.abs(top_left[1] - bottom_left[1])
-        zone_height = Math.max(zone_height, min_zoom_dimension)
-
+        let bottom_left = [most_left, most_bottom]
+        let bottom_right = [most_right, most_bottom]
+        let top_left = [most_left, most_top]
+        if (project) {
+            bottom_left = this.projection(bottom_left)
+            bottom_right = this.projection(bottom_right)
+            top_left = this.projection(top_left)
+        }
 
         const center = [(bottom_left[0] + bottom_right[0]) / 2, (bottom_left[1] + top_left[1]) / 2]
 
-        const scale_factor = Math.min(width / zone_width, height / zone_height) * 0.7
+        return [center, return_borders ? [bottom_left, bottom_right, top_left] : null]
+    }
 
-        const translate_x = width / 2 - center[0] * scale_factor
-        const translate_y = height / 2 - center[1] * scale_factor
+    addZoneTitle(zone, lat, long) {
+        const zone_title = d3.select(zone).data()[0].properties.NAME
+        this.g.append("text")
+            .attr("x", this.projection([lat, long])[0])
+            .attr("y", this.projection([lat, long])[1])
+            .attr("text-anchor", "middle")
+            .attr("id", "zone_name")
+            .text(zone_title)
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "20px")
+            .attr("fill", "black")
+            .style("pointer-events", "none")
+    }
+
+    removeZoneTitle() {
+        d3.select("#zone_name").remove()
+    }
+
+    zoomOnZone(zone) {
+        const result = this.getZoneCenter(zone, true, true)
+        const center = result[0]
+        const bottom_left = result[1][0]
+        const bottom_right = result[1][1]
+        const top_left = result[1][2]
+
+        let zone_width = Math.abs(bottom_right[0] - bottom_left[0])
+        zone_width = Math.max(zone_width, this.min_zoom_dimension)
+        let zone_height = Math.abs(top_left[1] - bottom_left[1])
+        zone_height = Math.max(zone_height, this.min_zoom_dimension)
+
+        const scale_factor = Math.min(this.svg_width / zone_width, this.svg_height / zone_height) * 0.7
+
+        const translate_x = this.svg_width / 2 - center[0] * scale_factor
+        const translate_y = this.svg_height / 2 - center[1] * scale_factor
 
         const transform = d3.zoomIdentity
             .translate(translate_x, translate_y)
             .scale(scale_factor)
 
-        svg.transition()
+            this.svg.transition()
             .duration(750)
-            .call(zoom.transform, transform)
+            .call(this.zoom.transform, transform)
     }
 
-    function zoomOut() {
-        svg.transition()
+    zoomOut() {
+        this.svg.transition()
             .duration(750)
-            .call(zoom.transform, d3.zoomIdentity)
+            .call(this.zoom.transform, d3.zoomIdentity)
     }
 
-    function resetZone(zone) {
-        console.log("Resetting zone", zone, is_zoomed)
-        fadeToColor(zone, default_zone_color)
-        removeZoneTitle()
+    resetZone(zone) {
+        this.fadeToColor(zone, this.default_zone_color)
+        this.removeZoneTitle()
     }
 }
 
@@ -225,17 +254,20 @@ swiss_country_data = "data/" + year + "/swissBOUNDARIES3D_1_3_TLM_LANDESGEBIET.g
 current_map_idx = 0
 maps = [swiss_districts_data, swiss_territory_data, swiss_canton_data, swiss_country_data]
 
+let map = null
+
 // run load_network() when page is loaded
 window.onload = function() {
-    load_map(data_file=maps[current_map_idx])
+    map = new SwissMap(maps[current_map_idx])
+    map.load_data()
 }
 
 function change_source() {
     // Delete old map
-    d3.select("svg").selectAll("*").remove()
+    //d3.select("svg").selectAll("*").remove()
     // Load new map
     current_map_idx = (current_map_idx + 1) % maps.length
-    load_map(data_file=maps[current_map_idx])
+    map.update_data(maps[current_map_idx])
 }
  
 document.onkeydown = function(e) {
