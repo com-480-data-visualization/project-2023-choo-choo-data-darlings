19f92730fcb9178587fbcb94b0b63ce7ae87f26b
ozan-guven <ozan.guven@epfl.ch>
2023-05-06 18:45:26 +0200

    Add hourly bar plot to home page

---
+++ website/src/HourlyBarPlot.ts
@@ -0,0 +1,190 @@
+import * as d3 from "d3";
+import data from "../data/hourly_plot/df_train.json";
+import { CirclePacking } from './CirclePacking.ts';
+import { HierarchicalEdgeBundling } from './HierarchicalEdgeBundling.ts';
+import './style.css';
+
+const BAR_WIDTH = 15
+const SHIFTED_MINUTES = 20
+const MINUTES_INTERVAL = 30
+const DATA_FOLDER = "../data/hourly_plot";
+
+const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+class HourlyBarPlot {
+    private svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
+    private data: any[] = data;
+    private parseTime = d3.timeParse("%H:%M:%S");
+    private xScale: d3.ScaleTime<number, number>;
+    private yScale: d3.ScaleLinear<number, number>;
+    private startTime: Date;
+    private endTime: Date;
+    private line: d3.Line<any>;
+    private bars: d3.Selection<SVGRectElement, unknown, HTMLElement, any>;
+    
+    constructor() {
+        // svg container
+        const svg = d3
+            .select("#chart")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+        this.data = data;
+        this.initializeData();
+        this.createLine();
+        this.createBars();
+        this.createAxes();
+    }
+
+    private initializeData(): void {
+        // Parse the time for each data point
+        this.data.forEach(d => {
+            d.departure_time = this.parseTime(d.departure_time);
+            d.departure_time.setYear(1970);
+        });
+        // Shift the start time to the next minute
+        const timeExtent = d3.extent(data, (d) => d.departure_time);
+        const startTime = new Date(timeExtent[0]);
+        startTime.setMinutes(startTime.getMinutes() - SHIFTED_MINUTES);
+
+        const endTime = new Date(timeExtent[1]);
+        endTime.setMinutes(endTime.getMinutes() + SHIFTED_MINUTES);
+        
+        // the scales
+        const xScale = d3
+            .scaleTime()
+            .domain([startTime, endTime])
+            .range([0, width]);
+        
+        const yScale = d3
+            .scaleLinear()
+            .domain([0, d3.max(data, (d) => d.count)])
+            .nice()
+            .range([height, 0]);
+    }
+
+    private createLine(): void {
+        // Create the line generator function
+        const line = d3
+            .line()
+            .x((d) => this.xScale(d.departure_time))
+            .y((d) => this.yScale(d.count));
+    }
+
+    private createBars(): void {
+        // Create the bar generator function
+        this.bars = this.svg
+            .selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", (d) => this.xScale(d.departure_time) - BAR_WIDTH / 2)
+            .attr("y", (d) => this.yScale(d.count))
+            .attr("width", BAR_WIDTH)
+            .attr("height", (d) => height - this.yScale(d.count))
+            .attr("fill", "steelblue");
+    }
+
+    private createAxes(): void {
+        this.svg
+            .append("g")
+            .attr("class", "x axis")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(this.xScale).tickFormat(d3.timeFormat("%H:%M")));
+        
+        this.svg
+            .append("g")
+            .attr("class", "y axis")
+            .call(d3.axisLeft(this.yScale));
+    }
+    
+    private updatePlot(transportMethod: string): void {
+        const data_path = `${DATA_FOLDER}/df_${transportMethod.toLowerCase().replace(' ', '_')}.json`;
+        d3.json(data_path).then((data: any) => {
+            // Parse the time for each data point
+            data.forEach((d: any) => {
+                d.departure_time = this.parseTime(d.departure_time);
+                d.departure_time.setYear(1970);
+            });
+    
+            // Update the xScale and yScale domain
+            const timeExtent = d3.extent(data, (d: any) => d.departure_time);
+            const startTime = new Date(timeExtent[0]);
+            startTime.setMinutes(startTime.getMinutes() - SHIFTED_MINUTES);
+    
+            const endTime = new Date(timeExtent[1]);
+            endTime.setMinutes(endTime.getMinutes() + SHIFTED_MINUTES);
+    
+            this.xScale.domain([startTime, endTime]);
+            this.yScale.domain([0, d3.max(data, (d) => d.count)]).nice();
+    
+            // Update the bars
+            this.bars = this.svg.selectAll(".bar")
+                .data(data);
+    
+            this.bars.join(
+                enter => enter.append("rect")
+                    .attr("class", "bar")
+                    .attr("x", (d) => this.xScale(d.departure_time) - BAR_WIDTH / 2)
+                    .attr("y", this.yScale(0)) // start from the bottom of the chart
+                    .attr("width", BAR_WIDTH)
+                    .attr("height", 0) // start with a height of 0
+                    .attr("fill", "steelblue"),
+                update => update
+                    .transition() // Start a transition
+                    .duration(1000) // Make it last 1 second
+                    .attr("x", (d) => this.xScale(d.departure_time) - BAR_WIDTH / 2)
+                    .attr("y", (d) => this.yScale(d.count))
+                    .attr("width", BAR_WIDTH)
+                    .attr("height", (d) => height - this.yScale(d.count))
+                    .attr("fill", transportMethod == "Train" ? "steelblue" : "red"),
+                exit => exit
+                    .transition() // Start a transition
+                    .duration(1000) // Make it last 1 second
+                    .attr("y", this.yScale(0)) // Move to the bottom of the chart
+                    .attr("height", 0) // End with a height of 0
+                    .remove() // After the transition, remove the bar
+            );
+    
+            // Update the axes
+            this.svg.select(".x.axis")
+                .transition()
+                .duration(1000)
+                .call(d3.axisBottom(this.xScale).tickFormat(d3.timeFormat("%H:%M")));
+    
+                this.svg.select(".y.axis")
+                .transition()
+                .duration(1000)
+                .call(d3.axisLeft(this.yScale));
+        });
+    }
+
+    private addButtons(): void {
+        // Define an array of cities
+        const transportTypes = ["Train", "Bus", "Boat", "Metro", "Tram", "Rack Railway"];
+
+        // create button for each type of transport
+        for (let i = 0; i < transportTypes.length; i++) {
+            const transportMethod = transportTypes[i];
+            const button = document.createElement("button");
+            button.setAttribute("type", "button");
+            button.setAttribute('id', transportMethod);
+            button.innerHTML = transportMethod;
+            button.onclick = function () {
+                this.updatePlot(transportMethod);
+            };
+
+            document.getElementById("buttons").appendChild(button);
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    new HierarchicalEdgeBundling();
+    new CirclePacking();
+});
