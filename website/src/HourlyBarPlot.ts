c1eb133f1127d20b8e0e866ef642ca07c34cb74d
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-05-13 18:39:40 +0200

    fix hour plot dimensions

--- website/src/HourlyBarPlot.ts
+++ website/src/HourlyBarPlot.ts
@@ -7,9 +7,12 @@ const SHIFTED_MINUTES = 20
 // const MINUTES_INTERVAL = 30
 const DATA_FOLDER = "data/hourly_plot";
 
+const BAR_ELEMENT_ID = "hourly-bar-chart";
+const BAR_BUTTONS_ELEMENT_ID = "hourly-bar-buttons";
+
 const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-const width = 960 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
+
+const BAR_WIDTH_FACTOR = 0.016;
 
 const COLOR_MAP = {
     "Train": "#00A59B",
@@ -35,16 +38,29 @@ export class HourlyBarPlot {
                    d3.Selection<d3.BaseType, { departure_time: string; count: number; }, SVGGElement, unknown>;
     
     constructor() {
+        this.initDimensions();
         this.initStaticElements();
         this.initPlot()
     }
 
+    /**
+     * Initializes the dimensions of the circle packing visualization.
+     * @returns {void}
+     */
+        initDimensions(): void {
+            const parentElement = document.getElementById(BAR_ELEMENT_ID);
+            if (parentElement) {
+                this.width = parentElement.clientWidth;
+                this.height = parentElement.clientHeight / 2;
+            }
+        }
+
     private initStaticElements(): void {
         this.svg = d3
-            .select("#chart")
+            .select(`#${BAR_ELEMENT_ID}`)
             .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
+            .attr("width", this.width + margin.left + margin.right)
+            .attr("height", this.height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
     }
@@ -77,13 +93,13 @@ export class HourlyBarPlot {
         this.xScale = d3
             .scaleTime()
             .domain([this.startTime, this.endTime])
-            .range([0, width]);
+            .range([0, this.width]);
         
         this.yScale = d3
             .scaleLinear()
             .domain([0, d3.max(this.data, (d) => d.count) as number])
             .nice()
-            .range([height, 0]);
+            .range([this.height, 0]);
     }
 
     private createLine(): void {
@@ -102,10 +118,10 @@ export class HourlyBarPlot {
             .enter()
             .append("rect")
             .attr("class", "bar")
-            .attr("x", (d) => this.xScale(d.departure_time) - BAR_WIDTH / 2)
+            .attr("x", (d) => this.xScale(d.departure_time) - (this.width * BAR_WIDTH_FACTOR) / 2)
             .attr("y", (d) => this.yScale(d.count))
-            .attr("width", BAR_WIDTH)
-            .attr("height", (d) => height - this.yScale(d.count))
+            .attr("width", this.width * BAR_WIDTH_FACTOR)
+            .attr("height", (d) => this.height - this.yScale(d.count))
             .attr("fill", "#00A59B");
     }
 
@@ -113,13 +129,17 @@ export class HourlyBarPlot {
         this.svg
             .append("g")
             .attr("class", "x axis")
-            .attr("transform", `translate(0,${height})`)
+            .attr("transform", `translate(0,${this.height})`)
             .call(d3.axisBottom(this.xScale).tickFormat(d3.timeFormat("%H:%M")));
         
         this.svg
             .append("g")
             .attr("class", "y axis")
             .call(d3.axisLeft(this.yScale));
+
+        this.svg
+            .selectAll(".tick")
+            .attr("font-size", "var(--small-font-size)")
     }
 
     private initPlot(): void {
@@ -161,11 +181,11 @@ export class HourlyBarPlot {
                         .attr("x", (d) => this.xScale(d.departure_time) - BAR_WIDTH / 2)
                         .attr("y", (d) => this.yScale(d.count))
                         .attr("width", BAR_WIDTH)
-                        .attr("height", (d) => height - this.yScale(d.count))
+                        .attr("height", (d) => this.height - this.yScale(d.count))
                         .attr("fill", COLOR_MAP[transportMethod])
                         // Change the color of the button elements to be the color of the bars
                         .each(function (d) {
-                            for (let button of document.getElementById("buttons")!.children) {
+                            for (let button of document.getElementById(BAR_BUTTONS_ELEMENT_ID)!.children) {
                                 button.setAttribute("style", `background-color: ${COLOR_MAP[transportMethod]}`);
                             }
                         }),
@@ -206,7 +226,7 @@ export class HourlyBarPlot {
                 this.updatePlot(transportMethod);
             };
 
-            document.getElementById("buttons")!.appendChild(button);
+            document.getElementById(BAR_BUTTONS_ELEMENT_ID)!.appendChild(button);
         }
     }
 }
