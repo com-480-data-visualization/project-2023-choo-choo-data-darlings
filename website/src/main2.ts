f45acaea015b85aac77119c05e85dd7f4983a652
ozan-guven <ozan.guven@epfl.ch>
2023-05-13 12:48:00 +0200

    Begin adding hierarchy

--- website/src/main2.ts
+++ website/src/main2.ts
@@ -1,5 +1,7 @@
 import { CirclePacking } from './CirclePacking.ts';
+import { HierarchicalEdgeBundling } from './HierarchicalEdgeBundling.ts';
 
 document.addEventListener("DOMContentLoaded", () => {
     new CirclePacking();
+    new HierarchicalEdgeBundling();
 });
