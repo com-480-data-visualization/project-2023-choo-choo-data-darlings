6e2850c69ebcc00ddcada75ac3bfc28443f3c673
ozan-guven <ozan.guven@epfl.ch>
2023-05-22 16:05:50 +0200

    Add working buttons

--- website/src/scroll.ts
+++ website/src/scroll.ts
@@ -96,4 +96,43 @@ document.addEventListener("DOMContentLoaded", () => {
   
   document.addEventListener("wheel", scrollDown, { passive: false });
   document.addEventListener("touchmove", scrollDown, { passive: false });
+  
+  // In scroll-buttons, add a listener when ckick on a button, scroll to the corresponding section
+  // The buttons are in li elements as:
+  /* <div id="scroll-buttons" class="dotstyle dotstyle-fillup">
+      <ul>
+        <li class="current" value="title-container"></li>
+        <li value="p1-container"></li>
+        <li value="p2-container"></li>
+        <li value="p3-container"></li>
+        <li value="p4-container"></li>
+        <li value="p5-container"></li>
+        <li value="p7-container"></li>
+        <li value="p8-container"></li>
+        <li value="final-container"></li>
+      </ul>
+    </div> */
+  const scrollButtons = document.getElementById("scroll-buttons");
+  if (scrollButtons) {
+    scrollButtons.addEventListener("click", (event) => {
+      const target = event.target as HTMLElement;
+      console.log(target)
+      const containers = [
+        "title-container",
+        "p1-container",
+        "p2-container",
+        "p3-container",
+        "p4-container",
+        "p5-container",
+        "p6-container",
+        "p7-container",
+        "p8-container",
+        "p9-container",
+        "final-container"
+      ];
+      const index = containers.indexOf(target.getAttribute("value")!);
+      const container = document.getElementById(containers[index]);
+      smoothScrollTo(container, SCROLL_DURATION);
+    });
+  }
 });
