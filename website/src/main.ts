5fcf76048bc70ea9609136fe7c44e92c3f0b24d6
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-05-26 11:48:38 +0200

    add fullpage and locked pages

--- website/src/main.ts
+++ website/src/main.ts
@@ -4,12 +4,76 @@ import { HourlyBarPlot } from './HourlyBarPlot.ts';
 import { GraphManager } from './network.ts';
 import { Map } from './map.ts';
 
+import fullpage from 'fullpage.js';
+import 'fullpage.js/dist/fullpage.css';
+
 const SCROLL_DURATION = 1000;
 
 document.addEventListener("DOMContentLoaded", () => {
     new CirclePacking();
     new HierarchicalEdgeBundling();
     new HourlyBarPlot();
+
+    // Fullpage.js
+    const sectionsLocked = [
+        false,  // title
+        false,  // introduction
+        true,   // circle packing
+        true,   // interactive table
+        true,   // hierarchical edge bundling
+        true,   // network introduction
+        true,   // network
+        true,   // hourly plot
+        true,   // map introduction
+        true,   // map
+        true,   // final
+    ];
+
+    new fullpage('#fullpage', {
+        navigation: true,
+        navigationTooltips: [
+            'Title',
+            'Introduction',
+            'Circle Packing',
+            'Interactive Table',
+            'Hierarchical Edge Bundling',
+            'Network Introduction',
+            'Network',
+            'Hourly Plot',
+            'Map Introduction',
+            'Map',
+            'Final'
+        ],
+        slidesNavigation: true,
+        controlArrows: false,
+        verticalCentered: false,
+        scrollingSpeed: 1000,
+        scrollOverflow: true,
+        onLeave: function(origin: any, destination: any, direction: any){
+            // if we are going forward and the destination is locked...
+            if(direction == 'down' && sectionsLocked[destination.index]) {
+                // don't let them leave the section (by canceling the move)
+                return false;
+            }
+            // otherwise, unlock the next section (if it exists)
+            else if(destination.index + 1 < sectionsLocked.length) {
+                sectionsLocked[destination.index + 1] = false;
+                navItems[destination.index + 1].classList.remove('locked');
+            }
+        }
+    });
+
+    var navItems = document.querySelectorAll('#fp-nav li');
+
+    // apply initial opacity
+    function applyOpacityToLockedSections() {
+        navItems.forEach(function(item, index) {
+            if (sectionsLocked[index]) {
+                item.classList.add('locked');
+            }
+        });
+    }
+    applyOpacityToLockedSections();
 
     // Load the graph when scoll to the p5-container div
     const p5Container = document.getElementById("p5-container");
